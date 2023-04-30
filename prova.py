7b653c2532570a698a09b1d50e08466a8a173966
RoboTuan <testingapplication96.@gmail.com>
2023-04-30 18:03:50 +0200

    Optimized data loading

--- prova.py
+++ prova.py
@@ -40,7 +40,7 @@ if not os.path.exists(knowledge_graph_file):
 parser = argparse.ArgumentParser(description='KRED')
 
 
-parser.add_argument('-c', '--config', default="./config.json", type=str,
+parser.add_argument('-c', '--config', default="./config.yaml", type=str,
                     help='config file path (default: None)')
 parser.add_argument('-r', '--resume', default=None, type=str,
                     help='path to latest checkpoint (default: None)')
@@ -49,7 +49,8 @@ parser.add_argument('-d', '--device', default=None, type=str,
 
 config = ConfigParser.from_args(parser)
 # print(config['data'])
-epochs = 5
+
+epochs = 1
 batch_size = 64
 train_type = "single_task"
 task = "user2item" # task should be within: user2item, item2item, vert_classify, pop_predict
@@ -58,5 +59,26 @@ config['trainer']['epochs'] = epochs
 config['data_loader']['batch_size'] = batch_size
 config['trainer']['training_type'] = train_type
 config['trainer']['task'] = task
+config['trainer']['save_period'] = epochs/2
+# The following parameters define which of the extensions are used, 
+# by setting them to False the original KRED model is executed 
+config['model']['use_mh_attention'] = False
+config['model']['mh_number_of_heads'] = 12
+config['data']['use_entity_category'] = True
+config['data']['use_second_entity_category'] = False
 
-data = load_data_mind(config)
+if not os.path.isfile(f"{config['data']['sentence_embedding_folder']}/train_news_embeddings.pkl"):
+    write_embedding_news("./data/train", config["data"]["sentence_embedding_folder"])
+
+if not os.path.isfile(f"{config['data']['sentence_embedding_folder']}/valid_news_embeddings.pkl"):
+    write_embedding_news("./data/valid", config["data"]["sentence_embedding_folder"])
+
+# with open("/Users/lpdef/Desktop/KRED/data/kg/wikidata-graph/entity2vecd100.vec") as f:
+#     print(next(f))
+#     for line in f:
+#         print(len(line.strip().split("\t")))
+#         break
+
+data = load_data_mind(config, config['data']['sentence_embedding_folder'])
+print("Data loaded, ready for training")
+# single_task_training(config, data)  # user2item
